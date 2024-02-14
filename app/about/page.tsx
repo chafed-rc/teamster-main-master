2257196e8ec9ed0d0e3a5b119a7016eb7b6b8059
chafed-rc <ryan.cuff@icloud.com>
2024-02-14 16:32:05 -0500

    Fix Apos

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,7 +13,7 @@ const AboutSection = () => {
             As we&apos;ve observed the tech landscape, it&apos;s become clear that even the most promising startups can falter, their potential curtailed by the challenges of project management and the struggle to maintain momentum. In response, we envisioned a solution — a tool that would not only streamline the complexities of development but also return command to the creators.
           </p>
           <p className="mb-4">
-            Our aspiration was to forge something beyond the conventional — a tool that is as agile and accessible as it is powerful. With Teamster, location and device constraints dissolve, giving way to a seamless, engaging experience that captivates and motivates. It's about more than mere management; it's about creating a space where projects thrive, where continuity is king, and progress is palpable.
+            Our aspiration was to forge something beyond the conventional — a tool that is as agile and accessible as it is powerful. With Teamster, location and device constraints dissolve, giving way to a seamless, engaging experience that captivates and motivates. It&apos;s about more than mere management; it&apos;s about creating a space where projects thrive, where continuity is king, and progress is palpable.
           </p>
           <p>
             Teamster isn&apos;t just an application; it&apos;s the pulse of productivity, the heartbeat of development projects. It&apos;s designed to invigorate and sustain your projects, transforming the way work unfolds. With Teamster, every venture is seen through to its fruition. We&apos;re not just building a tool; we&apos;re laying down a pathway to success, crafted for the journey of your projects from inception to completion.
