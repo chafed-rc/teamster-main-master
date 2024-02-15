2d924bc7890ceaa1bdbba1cfbd79bcefc3c24a53
chafed-rc <ryan.cuff@icloud.com>
2024-02-14 22:53:22 -0500

    updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,19 +3,19 @@ import React from 'react';
 const AboutSection = () => {
   return (
     <div className="text-white py-20 px-4 md:px-8 lg:px-16">
-      <div className="max-w-4xl mx-auto">
-        <h1 className="text-6xl font-bold mb-6">Teamster is giving you control of your projects.</h1>
-        <p className="text-lg mb-6 font-semibold">
+      <div className="max-w-4xl mx-auto space-y-6">
+        <h1 className="text-5xl font-bold mb-4 gradient-text">Empower Your Development with Teamster</h1>
+        <p className="text-lg mb-4 font-medium text-gray-300">
             We&apos;re dedicated to crafting a tool that empowers software developers with quality and control over their projects.
         </p>
-        <div className='text-gray-300'>
-          <p className="mb-4">
+        <div className='space-y-4'>
+          <p className="text-gray-400">
             As we&apos;ve observed the tech landscape, it&apos;s become clear that even the most promising startups can falter, their potential curtailed by the challenges of project management and the struggle to maintain momentum. In response, we envisioned a solution — a tool that would not only streamline the complexities of development but also return command to the creators.
           </p>
-          <p className="mb-4">
+          <p className="text-gray-400">
             Our aspiration was to forge something beyond the conventional — a tool that is as agile and accessible as it is powerful. With Teamster, location and device constraints dissolve, giving way to a seamless, engaging experience that captivates and motivates. It&apos;s about more than mere management; it&apos;s about creating a space where projects thrive, where continuity is king, and progress is palpable.
           </p>
-          <p>
+          <p className='text-gray-400'>
             Teamster isn&apos;t just an application; it&apos;s the pulse of productivity, the heartbeat of development projects. It&apos;s designed to invigorate and sustain your projects, transforming the way work unfolds. With Teamster, every venture is seen through to its fruition. We&apos;re not just building a tool; we&apos;re laying down a pathway to success, crafted for the journey of your projects from inception to completion.
           </p>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,18 @@ export default function Home() {
   return (
     <div className="mt-20 mx-auto px-4 md:mt-28">
       <div className="text-center md:max-w-2xl lg:max-w-3xl xl:max-w-4xl mx-auto">
-        <h1 className="text-4xl font-bold gradient-text lg:text-5xl xl:text-6xl">Teamster is the new way to build products</h1>
-        <p className="mt-10 text-md text-gray-300 lg:text-xl xl:text-2xl">
-          Streamline your way to success.
-          Elevate your project, achieve goals faster,
-          and simplify collaboration with ease. 
+        <h1 className="text-4xl font-bold text-[#5CB8E4] lg:text-5xl xl:text-6xl mb-4">Teamster</h1>
+        <p className="text-2xl text-white mb-10">
+          The new way to build products.
         </p>
-        <div className="flex  md:flex-row mt-7 lg:mt-10 items-center justify-center gap-4 md:gap-6">
-          <button className="bg-[#5CB8E4] border-2 border-transparent rounded-3xl text-xl text-white px-4 py-2  hover:border-[white] transition-colors duration-200 ease-in-out lg:text-2xl lg:px-8 lg:py-4">Learn More</button>
-          <button className="bg-[#5CB8E4] border-2 border-transparent rounded-3xl text-xl text-white px-4 py-2  hover:border-[white] transition-colors duration-200 ease-in-out lg:text-2xl lg:px-8 lg:py-4">Get Started</button>
+        <p className="mt-6 text-md text-gray-400 lg:text-xl xl:text-2xl mb-10">
+          Streamline your way to success. Elevate your project, achieve goals faster, and simplify collaboration with ease.
+        </p>
+        <div className="flex flex-col md:flex-row mt-7 lg:mt-10 items-center justify-center gap-6">
+          <button className="bg-transparent border-2 border-[#5CB8E4] text-[#5CB8E4] hover:bg-[#5CB8E4] hover:text-white transition-colors duration-300 ease-in-out rounded-3xl text-xl px-6 py-3 lg:text-2xl">Learn More</button>
+          <button className="bg-[#5CB8E4] text-white hover:opacity-90 transition-opacity duration-300 ease-in-out rounded-3xl text-xl px-6 py-3 lg:text-2xl">Get Started</button>
         </div>
-        <FeatureSection/>
+        <FeatureSection />
         <MiniFeatureSection />
       </div>
     </div>
